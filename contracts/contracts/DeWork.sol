96c12cbfef51a9b36bb9951351c83b4984f4e70f
Ashish-Kumar-007 <ashishsahoo2015@gmail.com>
2024-06-02 18:17:17 +0530

    Updated Contract code

--- contracts/contracts/DeWork.sol
+++ contracts/contracts/DeWork.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "./IncentiveToken.sol";
+
 // import "./SoulboundNFT.sol";
 
 contract DeWork {
@@ -12,7 +13,7 @@ contract DeWork {
 
     struct Employee {
         bool isEmployed;
-        bytes32 employeeDetailsHash;
+        string employeeDetailsHash;
         uint256 totalHours;
         uint256 tokensEarned;
     }
@@ -25,6 +26,7 @@ contract DeWork {
     mapping(address => Employee) private employees;
     mapping(address => CheckInOut[]) private employeeCheckInOut;
     mapping(address => uint256) private lastCheckInDate; // Stores the last check-in date for each employee
+    address[] private employeesList;
 
     event CheckIn(address indexed employee, uint256 timestamp);
     event CheckOut(address indexed employee, uint256 hoursWorked);
@@ -36,7 +38,18 @@ contract DeWork {
     }
 
     modifier onlyEmployee() {
-        require(employees[msg.sender].isEmployed, "Only employees can call this function");
+        require(
+            employees[msg.sender].isEmployed,
+            "Only employees can call this function"
+        );
+        _;
+    }
+
+    modifier both() {
+        require(
+            employees[msg.sender].isEmployed || msg.sender == admin,
+            "Not Authorized"
+        );
         _;
     }
 
@@ -46,70 +59,123 @@ contract DeWork {
         admin = msg.sender;
     }
 
-    function addEmployee(address employee, bytes32 employeeHash) external onlyAdmin {
+    function addEmployee(address employee, string calldata employeeHash)
+        external
+        onlyAdmin
+    {
         // CheckInOut memory checkInOut = new CheckInOut[](0);
         employees[employee] = Employee({
-            isEmployed: true, 
-            employeeDetailsHash: employeeHash, 
-            totalHours: 0, 
+            isEmployed: true,
+            employeeDetailsHash: employeeHash,
+            totalHours: 0,
             tokensEarned: 0
         });
+        employeesList.push(employee);
     }
 
     function removeEmployee(address employee) external onlyAdmin {
+        require(
+            employees[employee].isEmployed,
+            "Employee does not exist or already removed."
+        );
+
         employees[employee].isEmployed = false;
+
+        uint256 index = employeesList.length;
+        for (uint256 i = 0; i < employeesList.length; ++i) {
+            if (employeesList[i] == employee) {
+                index = i;
+                break;
+            }
+        }
+
+        require(index < employeesList.length, "Employee not found in list."); // Check if employee was found
+
+        // Move the last element into the place to delete
+        employeesList[index] = employeesList[employeesList.length - 1];
+        employeesList.pop(); // Remove the last element
     }
 
     function checkIn() external onlyEmployee {
-        require(_isValidCheckInTime(), "Check-in time must be at 10 am");
         require(_isNewDay(msg.sender), "You have already checked in today");
 
-        employeeCheckInOut[msg.sender].push(CheckInOut({
-            checkInTime: block.timestamp, 
-            checkOutTime: 0
-        }));
+        uint256 currentTime = block.timestamp % 1 days;
+        bool onTime = currentTime >= CHECK_IN_TIME &&
+            currentTime < CHECK_IN_TIME + 1 hours;
+        if (onTime) {
+            // Extra incentive tokens for on-time check-in
+            uint256 extraIncentive = 10 * 10**18; // Example: 10 extra tokens
+            employees[msg.sender].tokensEarned += extraIncentive;
+            emit RewardIssued(msg.sender, extraIncentive);
+        }
 
-        // Extra incentive tokens for on-time check-in
-        uint256 extraIncentive = 10 * 10 ** 18; // Example: 10 extra tokens
-        employees[msg.sender].tokensEarned += extraIncentive;
+        employeeCheckInOut[msg.sender].push(
+            CheckInOut({checkInTime: block.timestamp, checkOutTime: 0})
+        );
         lastCheckInDate[msg.sender] = block.timestamp / 1 days;
 
-        emit RewardIssued(msg.sender, extraIncentive);
         emit CheckIn(msg.sender, block.timestamp);
     }
 
-function checkOut() external onlyEmployee {
-    require(employeeCheckInOut[msg.sender].length > 0, "You must check in first");
-    CheckInOut storage lastCheckInOut = employeeCheckInOut[msg.sender][employeeCheckInOut[msg.sender].length - 1]; // Corrected access to employeeCheckInOut
-    require(lastCheckInOut.checkOutTime == 0, "Already checked out");
+    function checkOut() external onlyEmployee {
+        require(
+            employeeCheckInOut[msg.sender].length > 0,
+            "You must check in first"
+        );
+        CheckInOut storage lastCheckInOut = employeeCheckInOut[msg.sender][
+            employeeCheckInOut[msg.sender].length - 1
+        ]; // Corrected access to employeeCheckInOut
+        require(lastCheckInOut.checkOutTime == 0, "Already checked out");
 
-    uint256 hoursWorked = (block.timestamp - lastCheckInOut.checkInTime) / 1 hours;
-    employees[msg.sender].totalHours += hoursWorked;
-    lastCheckInOut.checkOutTime = block.timestamp;
+        uint256 hoursWorked = (block.timestamp - lastCheckInOut.checkInTime) /
+            1 hours;
+        employees[msg.sender].totalHours += hoursWorked;
+        lastCheckInOut.checkOutTime = block.timestamp;
 
-    uint256 reward = hoursWorked * 10 ** 18; // 1 hour = 1 token
-    employees[msg.sender].tokensEarned += reward;
+        uint256 reward = hoursWorked * 10**18; // 1 hour = 1 token
+        employees[msg.sender].tokensEarned += reward;
 
-    emit CheckOut(msg.sender, hoursWorked);
-    emit RewardIssued(msg.sender, reward);
-}
+        emit CheckOut(msg.sender, hoursWorked);
+        emit RewardIssued(msg.sender, reward);
+    }
 
     function mintIncentiveToken() external onlyEmployee {
-        uint tokensEarned = employees[msg.sender].tokensEarned;
+        uint256 tokensEarned = employees[msg.sender].tokensEarned;
         _token.mint(msg.sender, tokensEarned);
     }
 
-    function getEmployeeDetails(address employee) external view returns (Employee memory) {
+    function getEmployeeDetails(address employee)
+        external
+        view
+        both
+        returns (Employee memory)
+    {
         return employees[employee];
     }
 
-    function getCheckInOuts(address employee) external view returns (CheckInOut[] memory) {
+    function getAllEmployeeDetails()
+        external
+        view
+        onlyAdmin
+        returns (address[] memory)
+    {
+        return employeesList;
+    }
+
+    function getCheckInOuts(address employee)
+        external
+        view
+        onlyEmployee
+        returns (CheckInOut[] memory)
+    {
         return employeeCheckInOut[employee];
     }
 
     function _isValidCheckInTime() internal view returns (bool) {
         uint256 currentTime = block.timestamp % 1 days;
-        return currentTime >= CHECK_IN_TIME && currentTime < CHECK_IN_TIME + 1 hours;
+        return
+            currentTime >= CHECK_IN_TIME &&
+            currentTime < CHECK_IN_TIME + 1 hours;
     }
 
     function _isNewDay(address employee) internal view returns (bool) {
--- contracts/contracts/IncentiveToken.sol
+++ contracts/contracts/IncentiveToken.sol
@@ -4,11 +4,23 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract IncentiveToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("IncentiveToken", "INC") {
-        _mint(msg.sender, initialSupply);
+   
+    address private callerAddress;
+    address private owner; 
+    
+    constructor() ERC20("IncentiveToken", "INC") {
+        owner = msg.sender;
+
     }
 
     function mint(address to, uint256 amount) external {
+        require(callerAddress == msg.sender, "Not authorized");
         _mint(to, amount);
+    }
+
+    function setCallerAddress(address _addr) external  {
+        require(_addr != address(0), "Invalid address");
+        require(msg.sender == owner, "Not owner");
+        callerAddress = _addr;
     }
 }
